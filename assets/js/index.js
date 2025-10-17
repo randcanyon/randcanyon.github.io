2c5fbe51c4c917a3a7ab78afcb6a31e9f8cb805f
randy@clockworklabs.io <randy@clockworklabs.io>
2025-10-17 12:07:27 -0700

    Fixed post header and added scroll scripts

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,6 +13,16 @@ document.body.onmousemove = function(e) {
   );
 }
 
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
 /* 
 window.addEventListener('scroll', function() {
   const projects = document.getElementById('projects');
